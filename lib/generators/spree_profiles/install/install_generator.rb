471e70fcbf3bac8e64bf2e60ed27ef5c3333fc3c
Denis Gukov <denguk@gmail.com>
2015-09-22 09:30:11 +0500

    ...

--- lib/generators/spree_profiles/install/install_generator.rb
+++ lib/generators/spree_profiles/install/install_generator.rb
@@ -2,16 +2,6 @@ module SpreeProfiles
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_profiles\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_profiles\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_profiles\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_profiles\n", :before => /\*\//, :verbose => true
-      end
-
       def migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_profiles'
       end
